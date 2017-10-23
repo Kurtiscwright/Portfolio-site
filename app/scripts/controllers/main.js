91dd5509f3df7b7579e5af509d680ab0b889325b
Kurtis Wright <Kurtiscwright@users.noreply.github.com>
2017-10-22 22:42:32 -0700

    Added Yeoman/Grunt/Angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name portfolioSiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the portfolioSiteApp
+ */
+angular.module('portfolioSiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
